a32aede8387d19c675a04d1e788bd3367c145cac
hugo <hugof16silva@gmail.com>
2023-04-27 09:00:36 -0300

    inicio do projeto limpo

---
+++ src/modelo/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Modelo() {
+  return (
+    <div className="">
+      <h1>teste</h1>
+    </div>
+  );
+}
+
+
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function SignInEntrar() {
+  return (
+    <div className="">
+      <h1>SignInEntrar</h1>
+    </div>
+  );
+}
+
+
